ed57b71192850bc85e39306d55a821aec044d367
nherreraKx <nherrera@konexa.cl>
2026-06-16 11:11:44 -0400

    fix(backend): correct compression import to default import

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import * as compression from 'compression';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
